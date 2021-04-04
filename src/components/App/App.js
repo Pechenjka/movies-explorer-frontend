f4ad991c8c127a9807b4f8aaf68b4f4038b03cf2
Petr Lobachev <petya.lobachev@yandex.ru>
2021-04-04 13:35:54 +0300

    feat: добавить контекст для пользователя

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import { Route, Switch } from "react-router";
+import { useState } from "react";
+import "./App.css";
+import CurrentUserContext from "../../context/CurrentUserContext";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Movies from "../Movies/Movies";
-import { useState } from "react";
-import "./App.css";
 import SavedMovies from "../SavedMovies/SavedMovies";
 import NotFound from "../NotFound/NotFound";
 import Register from "../Register/Register";
@@ -12,6 +13,7 @@ import Login from "../Login/Login";
 import Profile from "../Profile/Profile";
 
 const App = () => {
+  const [currentUser, setCurrentUser] = useState({});
   const [loggedIn, setLoggedIn] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -23,38 +25,40 @@ const App = () => {
   };
 
   return (
-    <div className="page">
-      <Switch>
-        <Route exact path="/">
-          <Header />
-          <Main loggedIn={loggedIn} />
-          <Footer />
-        </Route>
-        <Route exact path="/movies">
-          <Header loggedIn={loggedIn} />
-          <Movies handleLoggidIn={handleLoggidIn} isLoading={isLoading} handleIsLoading={handleIsLoading} />
-          <Footer />
-        </Route>
-        <Route exact path="/saved-movies">
-          <Header loggedIn={loggedIn} />
-          <SavedMovies handleLoggidIn={handleLoggidIn} />
-          <Footer />
-        </Route>
-        <Route exact path="/signup">
-          <Register />
-        </Route>
-        <Route exact path="/signin">
-          <Login />
-        </Route>
-        <Route exact path="/profile">
-          <Header loggedIn={loggedIn} />
-          <Profile handleLoggidIn={handleLoggidIn} />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page">
+        <Switch>
+          <Route exact path="/">
+            <Header />
+            <Main loggedIn={loggedIn} />
+            <Footer />
+          </Route>
+          <Route exact path="/movies">
+            <Header loggedIn={loggedIn} />
+            <Movies handleLoggidIn={handleLoggidIn} isLoading={isLoading} handleIsLoading={handleIsLoading} />
+            <Footer />
+          </Route>
+          <Route exact path="/saved-movies">
+            <Header loggedIn={loggedIn} />
+            <SavedMovies handleLoggidIn={handleLoggidIn} />
+            <Footer />
+          </Route>
+          <Route exact path="/signup">
+            <Register />
+          </Route>
+          <Route exact path="/signin">
+            <Login />
+          </Route>
+          <Route exact path="/profile">
+            <Header loggedIn={loggedIn} />
+            <Profile handleLoggidIn={handleLoggidIn} />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </CurrentUserContext.Provider>
   );
 };
 
